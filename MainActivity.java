e40e59f793f4d54969dbc08e07499e0d86a999dc
yandripincay <yandripro.98@gmail.com>
2018-10-29 22:52:39 -0500

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.yandripincay.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("terceroB","Yandri Maikel Pincay Real ");
+        setContentView(R.layout.activity_main);
+    }
+}
